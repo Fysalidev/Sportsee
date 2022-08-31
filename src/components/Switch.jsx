93cdfc80963c288f364129ab20b669732a9a73a2
ROBILLIARD <82404725+Fysalidev@users.noreply.github.com>
2022-08-31 11:30:30 +0200

    Bouton Switch implémenté

--- src/components/Switch.jsx
+++ src/components/Switch.jsx
@@ -1,16 +1,56 @@
 import styled from "styled-components";
 
-const Wrapper = styled.div``;
+const SwitchButton = styled.label`
+  display: inline-block;
+  position: relative;
+  width: 70px;
+  height: 40px;
+  cursor: pointer;
+  overflow: hidden;
+`;
 
-const Title = styled.h1`
-  color: white;
+const Input = styled.input`
+  position: absolute;
+  top: -30px;
+  left: -30px;
+  width: 0;
+  height: 0;
+  & + span {
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    background: #99b4df;
+    border-radius: 20px;
+  }
+  &:checked + span {
+    background: #346abf;
+  }
+  & + span:before {
+    content: "";
+    display: inline-block;
+    position: absolute;
+    top: 50%;
+    left: 4px;
+    width: 32px;
+    height: 32px;
+    background: white;
+    border-radius: 50%;
+    transform: translateY(-50%);
+    transition: all 0.5s;
+  }
+  &:checked + span:before {
+    left: 34px;
+  }
 `;
 
 function Switch() {
   return (
-    <Wrapper>
-      <Title>Hello</Title>
-    </Wrapper>
+    <SwitchButton>
+      <Input type="checkbox" />
+      <span></span>
+    </SwitchButton>
   );
 }
 
