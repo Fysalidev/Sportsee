f9470617672960a15b8b026d5c014be4067d6269
ROBILLIARD <82404725+Fysalidev@users.noreply.github.com>
2022-09-02 14:36:12 +0200

    Layout Dashboard ok

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -12,22 +12,45 @@ const Main = styled.main`
   flex-grow: 1;
 `;
 
-const Content = styled.div`
-  margin:auto;
+const Content = styled.section`
+  margin: 68px 0 0 109px;
+  display: flex;
+  flex-direction: column;
+`;
+
+const Header = styled.header`
+  height: 166px;
+  font-family: "Roboto";
+  font-style: normal;
+  & h1 {
+    line-height: 24px;
+    font-size: 48px;
+    font-weight: 500;
+    padding: 1rem 0 2rem 0;
+  }
+  & span {
+    color: red;
+  }
+
+  & p {
+    line-height: 24px;
+    font-size: 18px;
+    font-weight: 400;
+  }
 `;
 
 const Activity = styled.div`
   display: flex;
-  max-width:1124px;
+  max-width: 1124px;
   justify-content: space-between;
 `;
 
 const Graphics = styled.div`
-flex-grow:1;
-max-width:834px;
-display:flex;
-flex-direction: column;
-gap: 28px;
+  flex-grow: 1;
+  max-width: 834px;
+  display: flex;
+  flex-direction: column;
+  gap: 28px;
 `;
 
 const Top = styled.div``;
@@ -38,28 +61,27 @@ const LargeGraph = styled.div`
 `;
 
 const Bottom = styled.div`
-display:flex;
-justify-content: space-between;
-
+  display: flex;
+  justify-content: space-between;
 `;
 
 const SmallGraph = styled.div`
-width:258px;
-height:263px;
-background:pink;
+  width: 258px;
+  height: 263px;
+  background: pink;
 `;
 
 const Cards = styled.div`
-background: yellow;
-display:flex;
-flex-direction:column;
-justify-content:space-between;
+  background: yellow;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
 `;
 
 const Card = styled.div`
-width:258px;
-height:124px;
-background:grey;
+  width: 258px;
+  height: 124px;
+  background: grey;
 `;
 
 function Dashboard() {
@@ -73,10 +95,12 @@ function Dashboard() {
       <VerticalLayout />
       <Main>
         <Content>
-          <div>
-            <h1>Bonjour Grégory</h1>
-            <p>Paragraphe de le page sera ici</p>
-          </div>
+          <Header>
+            <h1>
+              Bonjour <span>User {id}</span>
+            </h1>
+            <p>Félicitation ! Vous avez explosé vos objectifs hier 👏</p>
+          </Header>
           <Activity>
             <Graphics>
               <Top>
