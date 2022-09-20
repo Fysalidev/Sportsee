a08474827d4116c463d0813c4b06e7c843672ae0
ROBILLIARD <82404725+Fysalidev@users.noreply.github.com>
2022-09-20 11:08:51 +0200

    Version finale

--- src/components/ScoreGraph.jsx
+++ src/components/ScoreGraph.jsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 import PropTypes from "prop-types";
-import User from "../api/Models/User"
+import User from "../api/Models/User";
 import { ResponsiveContainer, RadialBarChart, RadialBar } from "recharts";
 
 const Wrapper = styled.div`
@@ -65,9 +65,6 @@ const Comment = styled.p`
  */
 
 function ScoreGraph({ data }) {
-
-  console.log(data)
-
   const tab = [
     {
       scale: 0,
